0009752ad0b3868075ae303d2c836dd875575d36
sachin <saanand@craterzone.com>
2019-03-08 18:04:46 +0530

    Second task

--- MentorzWork/TableViewCell.swift
+++ MentorzWork/TableViewCell.swift
@@ -12,13 +12,21 @@ protocol h {
 }
 class TableViewCell: UITableViewCell {
     var delegate : h?
+  let buttonBorderColor = UIColor(red: 146.0/255.0, green: 215.0/255.0, blue: 212.0/255.0, alpha: 1)
+    
     @IBOutlet weak var butotn: BorderColorButton!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var displayImage: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        butotn.buttonColor = UIColor.green
+        butotn.buttonColor = buttonBorderColor
+        butotn.setTitleColor(buttonBorderColor, for: .normal)
+        self.displayImage.layer.cornerRadius = self.displayImage.frame.size.width / 2
+        
+        self.displayImage.clipsToBounds = true
         
     }
     @IBAction func seemore(_ sender: Any) {
